7c5892c02c3edd070c9d0108833ca9f8af448602
Teedu <teet5858@gmail.com>
2018-12-26 16:47:51 +0200

    Parandused1

--- Heli.py
+++ Heli.py
@@ -9,25 +9,29 @@ def lask():
     heli_laskmine = pygame.mixer.Sound("lask.wav")
     heli_laskmine.play()
     
-def kõndimine(self):
+def success():
         
-    heli_kõndimine = pygame.mixer.Sound("")
-    heli_kõndimine.play() 
+    pygame.mixer.music.load("success.mp3")
+    pygame.mixer.music.play()
      
-def gameover(self):
+def gameover():
         
-    heli_gameover = pygame.mixer.Sound("")
-    heli_gameover.play() 
+    pygame.mixer.music.load("GameOVER.mp3")
+    pygame.mixer.music.play()
      
 def taustamuusika(self):
         
-    pygame.mixer.music.load("")
-    pygame.mixer.music.play()
+    pygame.mixer.music.load("taustamuusika.mp3")
+    pygame.mixer.music.play(self)
 
 while True:
     for i in pygame.event.get():
         if i.type==pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_SPACE:
+                taustamuusika(-1)
+            
 
-    lask()
+    
