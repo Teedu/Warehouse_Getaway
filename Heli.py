55006b15d67469a87d78c8b4db065fff1c8c1dbc
Teedu <teet5858@gmail.com>
2018-12-30 22:51:54 +0200

    kkommentaarid

--- Heli.py
+++ Heli.py
@@ -1,25 +1,20 @@
 import pygame, sys
 
 pygame.init()
-pygame.mixer.init()
-aken = pygame.display.set_mode([640, 480])
-
-def lask():
+pygame.mixer.init() #laadime mixeri
+aken = pygame.display.set_mode([640, 480]) #katsetuseks teeme ekraani
     
-    heli_laskmine = pygame.mixer.Sound("lask.wav")
-    heli_laskmine.play()
-    
-def success():
+def success(): #võidu helifaili funktsioon
         
-    pygame.mixer.music.load("success.mp3")
-    pygame.mixer.music.play()
+    pygame.mixer.music.load("success.mp3")#laeme sisse
+    pygame.mixer.music.play()#kanname ette
      
-def gameover():
+def gameover(): #mängu läbikukkumise helifaili funktsioon
         
     pygame.mixer.music.load("GameOVER.mp3")
     pygame.mixer.music.play()
      
-def taustamuusika(self):
+def taustamuusika(self): #taustamuusika helifaili funktsioon
         
     pygame.mixer.music.load("taustamuusika.mp3")
     pygame.mixer.music.play(self)
